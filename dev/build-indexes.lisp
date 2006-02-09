ee1f67c18b9b73f2a15c5fb2eb1abdf674e9f28a
Gary King <gwking@metabang.com>
2006-02-09 14:35:21 -0500

    Added permuted-index and reworked indexing code for additional flexibility
    darcs-hash:20060209193521-3cc5d-bfacc586a22faf581b35d9a8259c22384d07e331.gz

--- dev/build-indexes.lisp
+++ dev/build-indexes.lisp
@@ -20,6 +20,8 @@ DISCUSSION
     "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V"
     "W" "X" "Y" "Z"))
 
+;;; ---------------------------------------------------------------------------
+
 (defun build-index-of-kind (part subpart-info)
   (bind ((parts (item-at (subparts part) (name subpart-info)))
         (symbol-list (sort
@@ -40,43 +42,86 @@ DISCUSSION
                                 :force-contents-link? t)
           ((:a :name "top")
            (:h3 (lml-princ (heading subpart-info)) " Index"))
-          (:p
-            (dolist (letter symbol-list)
-              (if (some-key-value-p 
-                   parts
-                   (lambda (name part)
-                     (and 
-                      (string-equal (subseq (symbol-name name) 0 1) letter)
-                      (index-part-p part))))
-                   
-                (html ((:a :href (concatenate
-                                  'string 
-                                  "#" letter "-" (symbol-name (name subpart-info)))) 
-                       (lml-princ letter)))
-                (lml-format "~A" letter))
-              (lml-princ "&nbsp;&nbsp;")))
+          (build-index-letters symbol-list parts)
           
-          (iterate-container
-           parts
-           (lambda (part)
-             (when (index-part-p part)
-               (let ((part-name (part-name part)))
-                 (unless (string-equal current-symbol (subseq part-name 0 1))
-                   (setf current-symbol (subseq part-name 0 1))
-                   (html ((:a :name (concatenate 
-                                     'string current-symbol "-" 
-                                     (symbol-name (name subpart-info))))
-                          (:table
-                           (:tr ((:td :width 75 :valign "top" :align "left")
-                                 (:h3 (lml-princ (string-upcase current-symbol))))
-                                ((:td :valign "top" :align "right")
-                                 ((:a :href "#top") "Back to top")))))))
-                 (html (:p (display-part part :index))))))))))))
+          ((:div :class "index-contents")
+           (iterate-container
+            parts
+            (lambda (part)
+              (when (index-part-p part)
+                (let ((part-name (part-name part)))
+                  (unless (string-equal current-symbol (subseq part-name 0 1))
+                    (setf current-symbol (subseq part-name 0 1))
+                    (html ((:div :class "index-letter")
+                           ((:a :name (concatenate 
+                                       'string current-symbol "-section"))
+                            ((:div :class "the-letter") (lml-princ current-symbol))
+                            ((:div :class "back-to-top") 
+                             ((:a :href "#top") "Back to top"))))))
+                  (html (:p (display-part part :index)))))))))))))
+
+(defun build-index (part parts-to-index heading)
+  (bind ((symbol-list (sort
+                       (delete-duplicates
+                        (append *required-index-contents*
+                                (collect-elements 
+                                 parts-to-index :filter #'index-part-p
+                                 :transform (lambda (part) 
+                                              (string-upcase (subseq (part-name part) 0 1)))))
+                        :test #'string-equal)
+                       #'string-lessp))
+        (current-symbol nil))
+    (when (plusp (size parts-to-index))
+      (documenting-page (part :title (format nil "~:(~A~): Index of ~A"
+                                             (part-name part) heading)
+                              :force-contents-link? t)
+        ((:a :name "top")
+         (:h3 (lml-princ heading) " Index"))
+        (build-index-letters symbol-list parts-to-index)
+        
+        ((:div :class "index-contents")
+         (iterate-container
+          parts-to-index
+          (lambda (part)
+            (when (index-part-p part)
+              (let ((part-name (part-name part)))
+                (unless (string-equal current-symbol (subseq part-name 0 1))
+                  (setf current-symbol (subseq part-name 0 1))
+                  (html ((:div :class "index-letter")
+                         ((:a :name (concatenate 
+                                     'string current-symbol "-section"))
+                          ((:div :class "the-letter") (lml-princ current-symbol))
+                          ((:div :class "back-to-top") 
+                           ((:a :href "#top") "Back to top"))))))
+                (html (:p (display-part part :index))))))))))))
 
 ;;; ---------------------------------------------------------------------------
 
-(defmethod display-part ((part basic-doclisp-part) (mode (eql :index)))
-  (display-part-for-index part (part-name part)))
+(defun build-index-letters (symbol-list parts &key (key 'identity))
+  (html
+   ((:div :class "index-letters")
+    (dolist (letter symbol-list)
+      (if (some-element-p 
+           parts
+           (lambda (part)
+             (let ((part (funcall key part)))
+               (and 
+                (string-equal (subseq (symbol-name (name part)) 0 1) letter)
+                (index-part-p part)))))
+        
+        (html ((:div :class "used-letter")
+               ((:a :href (concatenate 'string "#" letter "-section")) 
+                (lml-princ letter))))
+        (html ((:div :class "dead-letter")
+               (lml-format "~A" letter))))))))
+
+;;; ---------------------------------------------------------------------------
+
+(defmethod display-part ((part basic-doclisp-part) (mode (eql :index))
+                         &key &allow-other-keys)
+  (html 
+   ((:div :class "index-name")
+    (display-part-for-index part (part-name part)))))
 
 ;;; ---------------------------------------------------------------------------
 
@@ -88,25 +133,48 @@ DISCUSSION
 
 ;;; ---------------------------------------------------------------------------
 
-(defmethod display-part ((part doclisp-symbol) (mode (eql :index)))
+(defmethod display-part ((part doclisp-symbol) (mode (eql :index))
+                          &key &allow-other-keys)
   (let ((name-holder (name-holder part)))
     (html
-     (:table
-      (:tr
-       ((:td :width 200) (display-part-for-index part (part-name part)))
-       (loop for kinds in (index-kinds (name-holder part)) do
-             (let ((real-part (some (lambda (kind)
-                                      (find-part name-holder kind (name part)))
-                                    kinds)))
-               (when real-part
-                 (html
-                  ((:td :width "20%")
-                   (display-part-for-index 
-                    real-part (string-downcase (part-kind real-part)))))))))))))
+     ((:div :class "index-name") (display-part-for-index part (part-name part)))
+     (iterate-elements
+      (sort (collect-keys (subparts (name-holder part))) #'string-lessp)
+      (lambda (kind)
+        (when (include-kind-in-index-p name-holder kind)
+          (bind ((real-part (find-part name-holder kind (name part))))
+            (when real-part
+              (html
+               ((:div :class "index-kind")
+                (display-part-for-index 
+                 real-part (string-downcase (part-kind real-part)))))))))))))
 
 ;;; ---------------------------------------------------------------------------
 
 (defmethod build-indexes (part)
+  (iterate-elements
+   (index-kinds part)
+   (lambda (index-description)
+     (bind (((part-kinds 
+              &key 
+              (heading (symbol->string (first part-kinds)))
+              (build-using #'build-index)
+              (index-name heading)
+              (index-kind (string->symbol index-name))) index-description))
+       (when (some-element-p part-kinds
+                             (lambda (part-kind)
+                               (index-for-kind-p part part-kind)))
+         (let ((*document-file* (index-file-name part index-name))
+               (*current-index* index-kind))
+           (with-new-file (*document-stream* *document-file*)
+             (funcall build-using part
+                      (loop for kind in part-kinds nconc
+                            (collect-elements
+                             (item-at (subparts part) kind)
+                             :filter #'index-part-p))
+                      heading))))))))
+#+Ignore
+(progn
   (map-subpart-kinds
    part
    (lambda (subpart-info)
@@ -118,25 +186,30 @@ DISCUSSION
                                             :if-exists :supersede
                                             :if-does-not-exist :create)
            
-           (build-index-of-kind part subpart-info)))))))
+           (build-index-of-kind part subpart-info))))))
+  
+  (let ((*document-file* (make-pathname 
+                          :name (format nil "permuted-index-~(~A~)"
+                                        (name part))
+                          :type "html"
+                          :defaults *document-root*))) 
+    (with-new-file (*document-stream* *document-file*)
+      (build-permuted-index part))))
 
 ;;; ---------------------------------------------------------------------------
 
-(defun index-file-name (part subpart-info) 
+(defun index-file-name (part subpart-name) 
   (declare (ignore part))
   (make-pathname 
-   :name (format nil "index-of-~A"
-                 (string-downcase (name subpart-info)))
+   :name (format nil "index-of-~(~A~)" subpart-name)
    :type "html"
    :defaults *document-root*))
 
 ;;; ---------------------------------------------------------------------------
 
-(defun index-url-name (part subpart-info) 
+(defun index-url-name (part subpart-name) 
   (make-root-pointing-url 
-   part 
-   (format nil "index-of-~A.html"
-           (string-downcase (name subpart-info)))))
+   part (namestring (pathname-name+type (index-file-name part subpart-name)))))
 
 ;;; ---------------------------------------------------------------------------
 
@@ -145,32 +218,40 @@ DISCUSSION
     (html
      ((:DIV :CLASS "index-links")
       ((:DIV :CLASS "index") "Indexes:")
-      (map-subpart-kinds
-       index-part
-       (lambda (subpart-info)
-         (when (index-for-kind-p index-part subpart-info)
-           (html
-            ((:DIV :CLASS "index")
-             (if (eq (name subpart-info) current-index)
-               (lml-format "~:(~A~)" (name subpart-info))
-               (html ((:a :href (index-url-name for-part subpart-info))
-                      (lml-format "~:(~A~)" (name subpart-info))))))))))))))
+      (iterate-elements
+       (index-kinds for-part)
+       (lambda (index-description)
+         (bind (((part-kinds 
+                  &key 
+                  (index-name (symbol->string (first part-kinds)))
+                  (index-kind (string->symbol index-name))
+                  &allow-other-keys) index-description))
+           (when (some-element-p part-kinds (lambda (kind) 
+                                              (index-for-kind-p for-part kind)))
+             (html
+              ((:DIV :CLASS "index")
+               (if (eq index-kind current-index)
+                 (lml-format "~:(~A~)" index-kind)
+                 (html ((:a :href (index-url-name for-part index-name))
+                        (lml-format "~:(~A~)" index-kind))))))))))))))
 
 ;;; ---------------------------------------------------------------------------
 
 (defun any-indexes-p (part)
-  (map-subpart-kinds
-   part
-   (lambda (subpart-info)
-     (when (index-for-kind-p part subpart-info)
-       (return-from any-indexes-p t)))))
+  (iterate-elements
+   (index-kinds part)
+   (lambda (index-description)
+     (bind (((part-kinds &key &allow-other-keys) index-description))
+       (when (some-element-p part-kinds (lambda (kind) 
+                                          (index-for-kind-p part kind)))
+         (return-from any-indexes-p t))))))
 
 ;;; ---------------------------------------------------------------------------
 
-(defun index-for-kind-p (part subpart-info)
-  (and (index? subpart-info)
+(defun index-for-kind-p (part kind)
+  (and (index? (subpart-info part kind))
        (some-element-p
-        (item-at (subparts part) (name subpart-info))
+        (item-at (subparts part) kind)
         #'index-part-p)))
 
 ;;; ---------------------------------------------------------------------------
---
+++ dev/permuted-index.lisp
@@ -0,0 +1,125 @@
+(in-package tinaa)
+
+(defun parts-matching-symbol (symbol-part name-holder)
+  "Returns a list of parts in name-holder than have the same name as symbol"
+  (let ((value nil))
+    (collect-elements 
+     (subparts name-holder)
+     :filter (lambda (subparts)
+               (let ((matching
+                      (search-for-item subparts (part-name symbol-part)
+                                       :key #'part-name)))
+                 (if (or (eq symbol-part matching)
+                         (part-can-have-documention-p matching))
+                   (setf value nil)
+                   (setf value matching))))
+     :transform (lambda (x) (declare (ignore x)) value))))
+
+;;; ---------------------------------------------------------------------------
+
+(defun build-permuted-index (main-part parts-to-index heading)
+  "A permuted index includes each n-word entry up to n times, at points corresponding to the use of each word in the entry as the sort key. For example, a symbol FOO-BAR would occur twice, once under FOO and BAR. This allows you to use any word in the symbol's name to search for that symbol."
+  (declare (ignore heading))
+  
+  (bind ((parts (loop for symbol-part in parts-to-index nconc
+                      (parts-matching-symbol symbol-part main-part)))
+         (symbol-list (sort
+                       (delete-duplicates
+                        (append *required-index-contents*
+                                (collect-elements 
+                                 parts-to-index 
+                                 :transform 
+                                 (lambda (part) 
+                                   (string-upcase (subseq (part-name part) 0 1)))))
+                        :test #'string-equal)
+                       #'string-lessp))
+         (current-letter nil))
+    (when (plusp (size parts))
+      (bind ((parts-to-index
+              (sort 
+               (loop for part in parts 
+                     when (index-part-p part) append
+                     `((,part 0)
+                       ,@(loop for index in (permuted-index-locations 
+                                             (name part) 'permuted-index-delimiter-p) collect
+                               (list part index))))
+               #'string-lessp
+               :key (lambda (part-index)
+                      (bind (((part index) part-index))
+                        (subseq (symbol-name (name part)) index)))))
+             (max-size (reduce-elements
+                        parts-to-index
+                        #'max
+                        :key (lambda (part.index)
+                               (bind (((part index) part.index))
+                                 (- (size (symbol-name (name part))) index))))))
+        (documenting-page (main-part :title (format nil "Permuted Index of ~A"
+                                                    (part-name main-part))
+                                     :force-contents-link? t)
+          ((:a :name "top")
+           (:h3 "Permuted Index of " (lml-princ (part-name main-part))))
+          (build-index-letters symbol-list parts-to-index :key #'car)
+          
+          ((:div :class "index-contents")
+           (iterate-container
+            parts-to-index
+            (lambda (part.index)
+              (bind (((part index) part.index)
+                     (part-name (part-name part))
+                     (ch (aref part-name index)))
+                (unless (and current-letter (char-equal current-letter ch))
+                  (setf current-letter ch)
+                  (html ((:div :class "index-letter")
+                         ((:a :name (format nil "~C-section" current-letter))
+                          ((:div :class "the-letter") (lml-princ current-letter))
+                          ((:div :class "back-to-top") 
+                           ((:a :href "#top") "Back to top"))))))
+                (display-part part :permuted-index :spaces (- max-size index)))))))))))
+
+#+Testing
+;;?? trying to use this iterator to not all the <pre></pre> stuff
+(let ((part-iterator (make-iterator parts-to-index))
+      (current-letter nil))
+  (loop while (move-forward-p part-iterator) do
+        (when (current-element-p part-iterator)
+          (bind (((part index) (current-element part-iterator))
+                 (part-name (part-name part))
+                 (ch (aref part-name index)))
+            (unless (and current-letter (char-equal current-letter ch))
+              (setf current-letter ch)
+              (html ((:div :class "index-letter")
+                     ((:a :name (format nil "~C-section" current-letter))
+                      ((:div :class "the-letter") (lml-princ current-letter))
+                      ((:div :class "back-to-top") 
+                       ((:a :href "#top") "Back to top"))))))
+            (display-part part :permuted-index :spaces (- max-size index))))
+        (move-forward part-iterator)))
+
+;;; ---------------------------------------------------------------------------
+
+(defun make-string-of-size (size initial-element)
+  (coerce (make-array size :initial-element initial-element) 'string))
+
+;;; ---------------------------------------------------------------------------
+
+(defun princ-n-spaces (stream n)
+  (loop repeat n do (princ " " stream)))
+
+;;; ---------------------------------------------------------------------------
+
+(defmethod display-part ((part basic-doclisp-part) (mode (eql :permuted-index))
+                         &key (spaces 0) &allow-other-keys)
+  (html
+   (:pre (lml-princ (make-string-of-size spaces #\ )) 
+         (display-part-for-index part (part-name part)))))
+
+;;; ---------------------------------------------------------------------------
+
+(defun permuted-index-delimiter-p (ch)
+  (char-equal ch #\-))
+
+;;; ---------------------------------------------------------------------------
+
+(defun permuted-index-locations (symbol delimiter-p)
+  (loop for x across (symbol-name symbol) 
+        for i = 0 then (1+ i) when (funcall delimiter-p x) collect (1+ i)))
