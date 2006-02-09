7d9e58008f56f91c6922e21adfde4f8fea76c4c9
Gary King <gwking@metabang.com>
2006-02-09 14:37:34 -0500

    Began test framework
    darcs-hash:20060209193734-3cc5d-9bd30784eff6587947037f77c3cd9bc880746867.gz

---
+++ unit-tests/tests.lisp
@@ -0,0 +1,56 @@
+(defpackage tinaa-tests
+  (:use "LIFT" "COMMON-LISP" "TINAA")
+  (:import-from "TINAA"
+                #:*document-root*
+                #:url->file))
+(in-package tinaa-tests)
+
+(defun pathnames-same-p (p1 p2)
+  (string-equal (namestring (translate-logical-pathname p1))
+                (namestring (translate-logical-pathname p2))))
+
+(deftestsuite test-url->file () ()
+  :teardown (setf *document-root* nil))
+
+(deftestsuite test-url->file-1 (test-url->file)
+  ()
+  :setup (setf *document-root* "user-home:docs;foo;"))
+
+(addtest (test-url->file-1)
+  (ensure-same (url->file "butter") "user-home:docs;foo;butter.html" 
+               :test 'pathnames-same-p))
+
+(addtest (test-url->file-1)
+  (ensure-same (url->file "better/butter") "user-home:docs;foo;better;butter.html" 
+               :test 'pathnames-same-p))
+
+(addtest (test-url->file-1)
+  (ensure-same (url->file "better/butter.text") "user-home:docs;foo;better;butter.html" 
+               :test 'pathnames-same-p))
+
+(addtest (test-url->file-1)
+  (ensure-same (url->file "better/butter.text" "htm") "user-home:docs;foo;better;butter.htm" 
+               :test 'pathnames-same-p))
+
+(addtest (test-url->file-1)
+  (ensure-same (url->file "better/butter.text") "user-home:docs;foo;better;butter.html" 
+               :test 'pathnames-same-p))
+
+(deftestsuite test-url->file-2 (test-url->file)
+  ()
+  :setup (setf *document-root* 
+               ;; MCL uses MacOS directory separators and adds volume name to namestring
+               #+DIGITOOL "Billy-Pilgrim:Users:gwking:docs:foo:"
+               #-DIGITOOL "/Users/gwking/docs/foo/"))
+
+(addtest (test-url->file-2)
+  (ensure-same (url->file "butter") "user-home:docs;foo;butter.html" 
+               :test 'pathnames-same-p))
+
+(addtest (test-url->file-2)
+  (ensure-same (url->file "better/butter.text") "user-home:docs;foo;better;butter.html" 
+               :test 'pathnames-same-p))
+
+;;; ---------------------------------------------------------------------------
+;;; relative-url
+;;; ---------------------------------------------------------------------------
